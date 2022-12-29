107d502c41359c1411ae8ce0b032e50807185e7f
sahilmandre <sahilmandre@gmail.com>
2022-12-29 17:18:44 +0530

    central chevron implemented

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
+  isChecked: boolean = false;
 
+  clicked(checked: boolean) {
+    this.isChecked = checked;
+  }
 }
